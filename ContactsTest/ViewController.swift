21ea4306b8049e48670a692b855959bf86973a3d
Y.Terada <y-terada@taguchinobumoto-no-macbook-pro.local>
2016-04-29 11:20:23 +0900

    Initial Commit

---
+++ ContactsTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContactsTest
+//
+//  Created by  Y.Terada on 2016/04/29.
+//  Copyright © 2016年  Y.Terada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
